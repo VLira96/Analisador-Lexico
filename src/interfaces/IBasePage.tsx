55326663e4990c4405695b40bce72df6cc4828ac
VLira96 <lira94634@gmail.com>
2024-04-09 14:08:50 -0300

    tirando um ; na interface

--- src/interfaces/IBasePage.tsx
+++ src/interfaces/IBasePage.tsx
@@ -1,5 +1,5 @@
 export default interface IBasePage {
-    titulo: string;
+    titulo: string
     alfabeto: string[]
     verificacao: (lista: string[], texto :string) => string
     listaParagrafo: string
